94a699ba1089140b0dbf356b08b9ce092c889ad1
unknown <ntang6@emory.edu>
2021-01-19 01:21:55 -0500

    typo1

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 docker build -t nevaehtang/multi-client:latest -t nevaehtang/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t nevaehtang/multi-server:latest -t nevaehtang/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t nevaehtang/multi-worker:latest -t nevaehtang/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
 docker push nevaehtang/multi-client:latest
 docker push nevaehtang/multi-server:latest
 docker push nevaehtang/multi-worker:latest
@@ -11,5 +12,5 @@ docker push nevaehtang/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=nevaehtang/multi-server:$SHA
-kubectl set iamge deployments/client-deployment client=nevaehtang/multi-client:$SHA
-kubectl set iamge deployments/worker-deployment worker=nevaehtang/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=nevaehtang/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=nevaehtang/multi-worker:$SHA
