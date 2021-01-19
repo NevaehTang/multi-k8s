be38bcd63d70e3eb1b814f047d2345199865160e
unknown <ntang6@emory.edu>
2021-01-19 18:35:53 -0500

    haiya

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Nevaeh's Fib Calculator! :)
+            Nevaeh's Fib Calculator!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
